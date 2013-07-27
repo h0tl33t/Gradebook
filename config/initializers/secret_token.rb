0d80f75abc8c2804b349c0598d1cdc3072dff661
Ryan Stenberg <h0tl33t@gmail.com>
2013-07-27 12:48:43 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gradebook::Application.config.secret_key_base = '3e1c8f51e88d37205d398d62e964f896242da9bc536972ac3e1fa1d71ec4d82577cfd63cb652e8f0fa76d52718573a39f127e5e831d83c7aead1ca526906a67c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gradebook::Application.config.session_store :cookie_store, key: '_gradebook_session'
